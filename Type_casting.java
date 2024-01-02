9df22a8d8f111b54bb814649ec5eb2d9172001ca
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-03 01:38:24 +0530

    Java program for converting a float value into an integer value.

---
+++ Type_casting.java
@@ -0,0 +1,7 @@
+public class Type_casting {
+    public static void main (String args[]) {
+        float a = 5.9826266f;
+        int b = (int) a;
+        System.out.println("After converting float into integer , value will be : " + b);
+    }
+}
