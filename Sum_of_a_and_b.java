44392d05eb1aeea3251858ba4a93332edbd85d70
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-03 01:36:42 +0530

    Java program for finding the sum of two number by taking input from user.

---
+++ Sum_of_a_and_b.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sum_of_a_and_b {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println("Sum : " + c);
+    }
+}
