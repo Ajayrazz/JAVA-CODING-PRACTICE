168f63cfe3aa0a9f0bdbc4ab2c3c755460229b45
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-09 18:40:42 +0530

    Java program to find the sum of two number by using method.

---
+++ MethodSumOfTwoNumber.java
@@ -0,0 +1,10 @@
+public class MethodSumOfTwoNumber {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static void main(String args[]){
+        int x = 4;
+        int y = 6;
+        System.out.println("Sum of two given number : "+sum(x,y));
+    }
+}
