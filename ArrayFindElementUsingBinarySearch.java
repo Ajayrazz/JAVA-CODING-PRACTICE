9b0d3450fda3ef1fb1ccb7bd95c1c29baa884346
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-13 19:40:54 +0530

    Java program to find the index of an element in the given array by using binary search.

---
+++ ArrayFindElementUsingBinarySearch.java
@@ -0,0 +1,24 @@
+public class ArrayFindElementUsingBinarySearch {
+    public static int binarySearch(int numbers[], int k){
+        int start = 0, end = numbers.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(numbers[mid] == k){
+                return mid;
+            }
+            if(numbers[mid] < k){
+                start = mid+1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int arr[] = {2,4,6,8,10,12,14};
+        int key = 4;
+        int index_value = binarySearch(arr,key);
+        System.out.println("Index of the "+key+" is : "+index_value);
+
+    }
+}
